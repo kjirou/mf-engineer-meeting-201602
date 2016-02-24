9b556620ed9821084bc01478f7ae13dfbaf7886b
kjirou <sorenariblog@gmail.com>
2016-02-24 10:13:52 +0900

    Prepare for the meeting

---
+++ index.js
@@ -0,0 +1,6 @@
+
+
+
+module.exports = {
+  foo: 1,
+};
---
+++ test/index.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+const indexModule = require('../index');
+
+
+describe('mf-engineer-meeting-201602', () => {
+
+  it('should be', () => {
+    assert(typeof indexModule, 'object');
+    assert.strictEqual(indexModule.foo, 1);
+  });
+});
